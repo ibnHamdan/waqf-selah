c818c383b24311c1af7fd38115aad0a9f532dd65
Abdulrahman <mac13@Abdulrahmans-MacBook-Pro.local>
2026-01-08 17:50:21 +0300

    edit base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/waqf-selah/',
+  // base: '/waqf-selah/',
   plugins: [
     TanStackRouterVite(),
     react(),
